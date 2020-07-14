1aa58da694e2efce72f849188f4359f1c72018f4
Bikash Roy <bikash0109@gmail.com>
2020-07-14 11:21:06 -0400

    Initial Setup pages, Work in progress
    React with npm

---
+++ src/components/create-watch.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateWatch extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Create Watch List Component!!</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/edit-watch.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditWatch extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Edit Watch List Component!!</p>
+            </div>
+        )
+    }
+}
